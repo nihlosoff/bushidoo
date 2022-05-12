a1db6b9f209230e17c974bd4c6f98770db71362d
petr <nihlosoff@gmail.com>
2022-05-12 16:56:31 +0300

    nuh

---
+++ src/components/Header.jsx
@@ -0,0 +1,6 @@
+const Header = () => {
+    return <header className='header'>
+        <img src="https://findicons.com/files/icons/767/wp_woothemes_ultimate/256/checkmark.png" alt=""/>
+    </header>
+}
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img
+                src="https://images.pexels.com/photos/457882/pexels-photo-457882.jpeg?cs=srgb&dl=sea-sky-beach-457882.jpg&fm=jpg"
+                alt=""/>
+        </div>
+        <div>
+            Ava Desc
+        </div>
+        <div>
+            My Posts
+            <div>
+                New post
+            </div>
+            <div>
+                Post 1
+            </div>
+            <div>
+                Post 2
+            </div>
+        </div>
+    </div>
+}
+export default Profile;
